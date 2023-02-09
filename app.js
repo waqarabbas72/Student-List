5c2e280c263caef77a4ce5e585ec58c26fa38c5b
waqarabbas72 <sheraliate751@gmail.com>
2023-02-09 23:37:22 +0500

    chnage

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 class Student {
-  constructor(sName, fName, sClass) {
+  constructor(sName, fName, sClass, id) {
     this.sName = sName;
     this.fName = fName;
     this.sClass = sClass;
+    this.id = id;
   }
 }
 
@@ -13,12 +14,12 @@ class UI {
     const row = document.createElement("tr");
     //Insert Cols
     row.innerHTML = `
-        <th scope="row">1</th>
+        <th>${gen.next().value}</th>
         <td>${std.sName}</td>
         <td>${std.fName}</td>
         <td>${std.sClass}</td>
         <td><a href="#"  class="text-decoration-none" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Edit</a></td>
-        <td><a href="#" class="delete">Remove</a></td>
+        <td><a href="#" class="delete" data-student-id="${std.id}">Remove</a></td>
         `;
     list.appendChild(row);
   }
@@ -28,29 +29,41 @@ class UI {
     if (target.className === "delete") {
       target.parentElement.parentElement.remove();
       // Remove From LS
-      Store.removeStudent(target.parentElement.previousElementSibling);
+      Store.removeStudent(target.attributes["data-student-id"].value);
     }
   }
 
   clearFields() {
-    document.getElementById("studentName").value = "";
-    document.getElementById("fatherName").value = "";
-    document.getElementById("class").value = "";
+      document.getElementById("studentName").value = "";
+      document.getElementById("fatherName").value = "";
+      document.getElementById("class").value = "";
+  }
+  
+}
+
+// Generator For student ID
+function* createIds(){
+  let index = 1;
+  while(true){
+    yield index++;
   }
 }
+const gen = createIds();
+
+// Instantiate UI In GLobal Scope
+const ui = new UI();
 
 // Event Listener For add Student
 document.getElementById("std-form").addEventListener("submit", (e) => {
   // Get Form Values
   const stdName = document.getElementById("studentName").value,
-    fatherName = document.getElementById("fatherName").value,
-    stdClass = document.getElementById("class").value;
+        fatherName = document.getElementById("fatherName").value,
+        stdClass = document.getElementById("class").value;
+        stdID = new Date().getTime();
 
   // Instantiate Student
-  const student = new Student(stdName, fatherName, stdClass);
+  const student = new Student(stdName, fatherName, stdClass, stdID);
   console.log(student);
-
-  const ui = new UI();
 
   //Validate
   if (stdName === "" || fatherName === "" || stdClass === "") {
@@ -63,13 +76,12 @@ document.getElementById("std-form").addEventListener("submit", (e) => {
     // Store in LS
     Store.addStudent(student);
   }
-
+  
   e.preventDefault();
 });
 
 // Local Storage Class
 class Store {
-
   // GetStudents data
   static getStudents() {
     let students;
@@ -85,9 +97,7 @@ class Store {
   //Add Students Data
   static addStudent(student) {
     const students = Store.getStudents();
-
     students.push(student);
-
     localStorage.setItem("students", JSON.stringify(students));
   }
 
@@ -96,32 +106,27 @@ class Store {
     const students = Store.getStudents();
 
     students.forEach(function (student) {
-      const ui = new UI();
       // Add Book to list
       ui.addStudentToList(student);
     });
   }
 
   // Remove Students Data
-  static removeStudent() {
+  static removeStudent(id) {
     const students = Store.getStudents();
-
-    students.forEach((index) => {
-      students.splice(index,1);
-    });
+    const index = students.findIndex((s) => s.id == id);
+          students.splice(index, 1);
 
     localStorage.setItem("students", JSON.stringify(students));
   }
 }
+
 
 // DOM Load Event
 document.addEventListener("DOMContentLoaded", Store.dispalyStudents);
 
 //Event Listener For Delete Student
 document.getElementById("stdList").addEventListener("click", function (e) {
-  // Instantiate UI
-  const ui = new UI();
-
   // Delete Student
   ui.deleteStd(e.target);
 });
